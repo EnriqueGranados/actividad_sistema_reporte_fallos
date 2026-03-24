a20bf2f221ec960cdf49be361ea318a1d05c7139
Enrique Granados <jkike2001@gmail.com>
2026-03-24 01:10:00 -0600

    Corrección del script de inicialización para despliegue secuencial

--- sql-init/main.sql
+++ sql-init/main.sql
@@ -2,9 +2,9 @@
 -- Inicialización (MAIN).
 -- =======================================================================
 
-\i V1_20260323_create_tables.sql
-\i V1_20260323_create_functions_triggers.sql
-\i V1_20260323_alter_tables.sql
-\i V1_20260323_create_index.sql
-\i V1_20260324_vacuum_analyze.sql
-\i V1_20260324_comment_on.sql
+\i /docker-entrypoint-initdb.d/V1_20260323_create_tables.sql
+\i /docker-entrypoint-initdb.d/V1_20260323_create_functions_triggers.sql
+\i /docker-entrypoint-initdb.d/V1_20260323_alter_tables.sql
+\i /docker-entrypoint-initdb.d/V1_20260323_create_index.sql
+\i /docker-entrypoint-initdb.d/V1_20260324_vacuum_analyze.sql
+\i /docker-entrypoint-initdb.d/V1_20260324_comment_on.sql
