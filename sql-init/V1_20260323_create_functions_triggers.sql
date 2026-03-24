0cdfb48fb0d8c7bf1362385a36cbc6cda748d480
Enrique Granados <jkike2001@gmail.com>
2026-03-23 23:38:14 -0600

    Agregando función y trigger de automatización

---
+++ sql-init/V1_20260323_create_functions_triggers.sql
@@ -0,0 +1,36 @@
+-- =======================================================================
+-- Módulo 1: Automatización mediante Triggers y PL/pgSQL.
+-- =======================================================================
+
+--Función para actualizar el estado de la pizarra.
+CREATE OR REPLACE FUNCTION actualizar_estado_pizarra()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE pizarras
+    SET estado = 'En Reparación' 
+    WHERE id_pizarra = NEW.id_pizarra;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger asociado a la función de tipo AFTER INSERT.
+CREATE TRIGGER trg_cambio_estado_pizarra
+AFTER INSERT ON reportes_fallos 
+FOR EACH ROW 
+EXECUTE FUNCTION actualizar_estado_pizarra();
+
+/*
+Justificación Técnica
+
+FOR EACH ROW: Es estrictamente necesario para garantizar que si 
+una sola transacción inserta múltiples registros de fallos 
+simultáneamente, la función se ejecute de manera individual 
+para evaluar y actualizar el estado de cada pizarra específica 
+afectada.
+
+Timing AFTER: Es preferible utilizar AFTER para asegurar que el 
+nuevo reporte de fallo ha pasado todas las validaciones e 
+inserciones en su propia tabla antes de propagar el cambio de 
+estado a la tabla de activos, es decir, solo cuando ese 
+reporte ya es oficial, le cambiamos el estado a la pizarra.
+*/
