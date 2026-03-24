5f94e94b82fbfd17b24ba2315b54d572f5468f73
Enrique Granados <jkike2001@gmail.com>
2026-03-24 00:43:01 -0600

    Agregando script de inicialización para despliegue secuencial

---
+++ sql-init/main.sql
@@ -0,0 +1,10 @@
+-- =======================================================================
+-- Inicialización (MAIN).
+-- =======================================================================
+
+\i V1_20260323_create_tables.sql
+\i V1_20260323_create_functions_triggers.sql
+\i V1_20260323_alter_tables.sql
+\i V1_20260323_create_index.sql
+\i V1_20260324_vacuum_analyze.sql
+\i V1_20260324_comment_on.sql
